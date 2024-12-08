4f3b4b059a55f27b4e747d21a84a57c3ecd2b581
tuyenstar121 <tuyen2k10@gmail.com>
2024-12-08 20:44:47 +0700

    Save changes to stafforder.js

--- src/components/stafforder.js
+++ src/components/stafforder.js
@@ -4,7 +4,8 @@ import Cookies from "js-cookie";
 import { toast } from "react-toastify";
 import Menu from "../components/Menu/menu";
 import MenuEdit from "../components/Menu/Menuedit";
-import RestaurantSelector from "./RestaurantSelector";
+// import RestaurantSelector from "./RestaurantSelector";
+// import {InvoicePDF} from './InvoicePDF';
 import Invoice from "./Invoice";
 import {
   Dialog,
@@ -14,6 +15,8 @@ import {
   Button,
 } from "@mui/material";
 import ConfirmDialog from "./ConfirmDialog";
+import { json } from "react-router-dom";
+
 const CombinedLayout = () => {
   const [cart, setCart] = useState(() => {
     return JSON.parse(localStorage.getItem("cart")) || [];
@@ -29,8 +32,11 @@ const CombinedLayout = () => {
   const [loading, setLoading] = useState(false); // Trạng thái tải
   const [newTableId, setNewTableId] = useState(null);
   const [switchingTable, setSwitchingTable] = useState(false);
- 
+  const [isPrinted, setIsPrinted] = useState(false);
   const [showConfirmDialog, setShowConfirmDialog] = useState(false);
+  const [promotionToday, setPromotionToday] = useState([]);
+  const [error, setError] = useState("");
+  const restaurantId=1;
   // Đồng bộ hóa giỏ hàng với localStorage
   useEffect(() => {
     localStorage.setItem("cart", JSON.stringify(cart));
@@ -55,12 +61,21 @@ const CombinedLayout = () => {
   }, []);
 
   useEffect(() => {
-   
-      fetchTablesByRestaurant(1);
-    }
-  );
+    fetchPromotionToday();
+    fetchTablesByRestaurant(1);
+  }, []);
+  const fetchPromotionToday = async () => {
+    try {
+      const response = await axios.get('http://localhost:8080/api/promotions/by-date?date=' + new Date().toISOString().split('T')[0]);
 
-  // Lấy thông tin đặt bàn
+      const items = response.data;
+
+      setPromotionToday(items)
+    } catch (error) {
+      console.error("There was an error fetching the menu items!", error);
+      setError("Lỗi tải dữ liệu khuyến mãi");
+    }
+  };
   const fetchReservationDetails = useCallback(async (reservationId) => {
     try {
       const token = Cookies.get("token");
@@ -69,18 +84,18 @@ const CombinedLayout = () => {
         { headers: { Authorization: `Bearer ${token}` } }
       );
       const { restaurantId, table } = response.data;
-  
+
       // Update reservation details and other states
       setReservationDetails(response.data); // Save the entire reservation details
       setSelectedRestaurant(restaurantId);
       setSelectedTable({ id: table.id, number: table.tableNumber });
-  
+
     } catch (error) {
       console.error("Error fetching reservation details:", error);
       toast.error("Không thể lấy thông tin đặt bàn!");
     }
   }, []);
-  
+
   const fetchFoodItems = async (reservationId) => {
     const token = Cookies.get('token');
     try {
@@ -92,7 +107,7 @@ const CombinedLayout = () => {
 
     } catch (error) {
       console.error("There was an error fetching the food items!", error);
-    
+
     }
   };
   useEffect(() => {
@@ -106,39 +121,8 @@ const CombinedLayout = () => {
     setSelectedTable(null);
   };
   // Xử lý thêm/xóa món ăn trong giỏ hàng
-  const updateCart = useCallback((updatedCart) => {
-    setCart(updatedCart);
-  }, []);
 
-  const incrementQuantity = useCallback(
-    (id) => {
-      const updatedCart = cart.map((item) =>
-        item.id === id ? { ...item, quantity: item.quantity + 1 } : item
-      );
-      updateCart(updatedCart);
-    },
-    [cart, updateCart]
-  );
 
-  const decrementQuantity = useCallback(
-    (id) => {
-      const updatedCart = cart.map((item) =>
-        item.id === id && item.quantity > 1
-          ? { ...item, quantity: item.quantity - 1 }
-          : item
-      );
-      updateCart(updatedCart);
-    },
-    [cart, updateCart]
-  );
-
-  const removeFromCart = useCallback(
-    (id) => {
-      const updatedCart = cart.filter((item) => item.id !== id);
-      updateCart(updatedCart);
-    },
-    [cart, updateCart]
-  );
   const toggleSwitchingTable = () => {
     setSwitchingTable(!switchingTable);
     if (!switchingTable) toast.info("Chọn bàn mới để chuyển đến.");
@@ -158,12 +142,14 @@ const CombinedLayout = () => {
 
       if (response.status === 200) {
         toast.success("Chuyển bàn thành công!");
-        const updatedReservation = response.data;
+        const updatedReservation = response.data.updatedReservation;
+        
         setSelectedTable({
-          id: updatedReservation.table.id,
+          id: updatedReservation.table.tableId,
           number: updatedReservation.table.tableNumber,
         });
-        fetchTablesByRestaurant(selectedRestaurant);
+        
+        fetchTablesByRestaurant(1);
       } else {
         toast.error("Không thể chuyển bàn!");
       }
@@ -172,12 +158,15 @@ const CombinedLayout = () => {
       toast.error("Đã xảy ra lỗi khi chuyển bàn!");
     }
   };
-  const handleSwitchTable = async (newTableId) => {
+  const handleSwitchTable = async () => {
     setShowConfirmDialog(true);
   };
   const handleTableClick = (table) => {
     if (switchingTable) {
       if (table.status === "available") {
+        
+        setSelectedTable({ id: table.tableId, number: table.tableNumber });
+        console.log(selectedTable); 
         setNewTableId(table.tableId);
         handleSwitchTable(table.tableId); // Gọi API đổi bàn
       } else {
@@ -185,20 +174,16 @@ const CombinedLayout = () => {
       }
     } else {
       if (table.status === "available") {
-        setSelectedTable({ id: table.id, number: table.tableNumber });
+       
+        setSelectedTable({ id: table.tableId, number: table.tableNumber });
+     
         setShowMenuTable(true);
       } else {
         toast.warn("Bàn này đã có khách!");
       }
     }
   };
-function getDiscountPercentage(itemId, promotions) {
-  if (!promotions || promotions.length === 0) {
-    return 0; // Không có khuyến mãi
-  }
-  const promotion = promotions.find((p) => p.itemId === itemId);
-  return promotion ? promotion.discount : 0;
-}
+ 
   const handlePaymentClick = () => setShowInvoice(true);
   const handleCloseInvoice = () => setShowInvoice(false);
 
@@ -211,21 +196,21 @@ function getDiscountPercentage(itemId, promotions) {
 
       {/* Right-Side: Cart and Table Management */}
       <div className="w-full md:w-1/3 p-4 bg-gray-50 overflow-auto">
-        <RestaurantSelector setSelectedRestaurant={setSelectedRestaurant} />
+
         {reservationId && (
-  <div className="bg-blue-100 p-4 rounded mb-4">
-    <h3 className="text-lg font-bold">
-      Đơn đặt bàn: {reservationId}
-    </h3>
-    <h2 className="text-xl font-bold mb-4">
-    {reservationDetails?.table ? (
-  <p>Bàn: {reservationDetails.table.tableNumber}</p>
-) : (
-  <p>Chưa có thông tin bàn</p>
-)}
+          <div className="bg-blue-100 p-4 rounded mb-4">
+            <h3 className="text-lg font-bold">
+              Đơn đặt bàn: {reservationId}
+            </h3>
+            <h2 className="text-xl font-bold mb-4">
+              {reservationDetails?.table ? (
+                <p>Bàn: {reservationDetails.table.tableNumber}</p>
+              ) : (
+                <p>Chưa có thông tin bàn</p>
+              )}
             </h2>
-  </div>
-)}
+          </div>
+        )}
         {!showMenuTable ? (
           <div>
             <h2 className="text-xl font-bold">Chọn bàn</h2>
@@ -233,18 +218,16 @@ function getDiscountPercentage(itemId, promotions) {
               {tables.map((table) => (
                 <div
                   key={table.id}
-                  className={`w-full h-24 border rounded-lg shadow-md flex flex-col ${
-                    table.status === "available" ? "cursor-pointer" : "cursor-not-allowed"
-                  }`}
+                  className={`w-full h-24 border rounded-lg shadow-md flex flex-col ${table.status === "available" ? "cursor-pointer" : "cursor-not-allowed"
+                    }`}
                   onClick={() => handleTableClick(table)}
                 >
                   <div className="flex-1 flex items-center justify-center bg-white rounded-t-lg">
                     <span className="text-lg font-bold">{table.tableNumber}</span>
                   </div>
                   <div
-                    className={`flex-1.1 flex items-center justify-center rounded-b-lg ${
-                      table.status === "available" ? "bg-green-500" : "bg-gray-400"
-                    }`}
+                    className={`flex-1.1 flex items-center justify-center rounded-b-lg ${table.status === "available" ? "bg-green-500" : "bg-gray-400"
+                      }`}
                   >
                     <span className="text-white font-medium">
                       {table.status === "available" ? "Bàn trống" : "Có khách"}
@@ -259,12 +242,7 @@ function getDiscountPercentage(itemId, promotions) {
             <h2 className="text-xl font-bold mb-4">
               Bàn: {selectedTable?.number}
             </h2>
-            <MenuEdit
-  foodItems={cart || []} // Đảm bảo cart không phải là undefined
-  onRemove={removeFromCart}
-  onIncrement={incrementQuantity}
-  onDecrement={decrementQuantity}
-/>
+            <MenuEdit reservationId={reservationId} promotionToday={promotionToday} />
             <button
               className="mt-4 px-4 py-2 bg-red-500 text-white rounded"
               onClick={handleBackToTableSelection}
@@ -275,9 +253,7 @@ function getDiscountPercentage(itemId, promotions) {
         )}
 
         <div className="flex space-x-4 mt-4">
-          <button className="flex items-center px-3 py-2 bg-gray-700 text-white font-bold rounded-full shadow-lg hover:shadow-gray-800 transition duration-300">
-            <i className="fas fa-print mr-1"></i> In hóa đơn
-          </button>
+         
           <button
             className="flex items-center px-3 py-2 bg-green-500 text-white font-bold rounded-full shadow-lg hover:shadow-green-600 transition duration-300"
             onClick={handlePaymentClick}
@@ -288,19 +264,18 @@ function getDiscountPercentage(itemId, promotions) {
             <i className="fas fa-compress-alt mr-1"></i> Gộp bàn
           </button>
           <button
-  className={`flex items-center px-3 py-2 ${
-    switchingTable ? "bg-red-500" : "bg-purple-600"
-  } text-white font-bold rounded-full shadow-lg hover:shadow-purple-800 transition duration-300`}
-  onClick={toggleSwitchingTable}
->
-  <i className="fas fa-arrows-alt mr-1"></i> {switchingTable ? "Hủy chuyển bàn" : "Chuyển bàn"}
-</button>
+            className={`flex items-center px-3 py-2 ${switchingTable ? "bg-red-500" : "bg-purple-600"
+              } text-white font-bold rounded-full shadow-lg hover:shadow-purple-800 transition duration-300`}
+            onClick={toggleSwitchingTable}
+          >
+            <i className="fas fa-arrows-alt mr-1"></i> {switchingTable ? "Hủy chuyển bàn" : "Chuyển bàn"}
+          </button>
         </div>
       </div>
       {showConfirmDialog && (
         <ConfirmDialog
           title="Xác nhận chuyển bàn"
-          message={`Bạn có chắc chắn muốn chuyển đến bàn ${selectedTable?.number}}?`}
+          message={`Bạn có chắc chắn muốn chuyển đến bàn ${selectedTable?.number}?`}
           onConfirm={executeSwitchTable}
           onCancel={() => setShowConfirmDialog(false)}
         />
@@ -309,7 +284,7 @@ function getDiscountPercentage(itemId, promotions) {
       <Dialog open={showInvoice} onClose={handleCloseInvoice} maxWidth="md" fullWidth>
         <DialogTitle>Hóa Đơn</DialogTitle>
         <DialogContent>
-          <Invoice reservation={reservationDetails} items={foodItems}/>
+          <Invoice reservation={reservationDetails} items={foodItems} />
         </DialogContent>
         <DialogActions>
           <Button onClick={handleCloseInvoice} color="primary">
